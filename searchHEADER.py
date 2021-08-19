f0fab4492969eddece20748c2a502b9b70907db4
KingOfTips <kingofbugbounty@gmail.com>
2021-08-19 13:26:24 -0300

    Example Parser

--- searchHEADER.py
+++ searchHEADER.py
@@ -1,22 +1,23 @@
-#Search Headers DoD Priv8
-#@OFJAAAH 
+# Search Headers DoD Priv8
+# @OFJAAAH
 
 import requests
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 import sys
 import argparse
 
-#Import File
-
-file = sys.argv[1]
+#Parser
+parser = argparse.ArgumentParser()
+parser.add_argument("help", help="Run to code = python3 searchHEADER.py FileToUrls ")
+args = parser.parse_args()
 
 def headersURL():
-    
+
     with open(file, 'r') as arquivo:
         dominios = arquivo.read().split('\n')[:-1]
     with open('results.txt', 'w') as fp:
-      
-      for url in dominios:
+       
+        for url in dominios:
             headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
             try:
                 r = requests.get(url, headers=headers)
@@ -24,4 +25,12 @@ def headersURL():
                 fp.write(f"{r.headers['server'] if r.headers['server'] else 'none'}:{url}\n")
             except:
                 pass
-headersURL()
+
+if __name__ == "__main__":
+    try:
+        file = sys.argv[1]
+        headersURL()
+
+    except Exception as e:
+        print(f"{Fore.RED}[!]{Style.RESET_ALL} Please pass a file with url's as argument")
+        exit(-1)
