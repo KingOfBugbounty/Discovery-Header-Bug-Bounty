397c2594b9a2450d1ac591510950e2bc8d483349
kevin Lyon <blackmaskdeveloper@gmail.com>
2021-08-20 14:30:55 -0300

    add threads, define output and add description in ArgumentParser

--- searchHEADER.py
+++ searchHEADER.py
@@ -4,39 +4,60 @@
 
 import requests
 from colorama import Fore, Style
-import sys
+from threading import Thread
 import argparse
 
 # Parser
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="Run to code = python3 searchHEADER.py FileToUrls ")
 parser.add_argument(
-    "help", help="Run to code = python3 searchHEADER.py FileToUrls ")
+    "-f", "--file", metavar="", help="define a file")
+parser.add_argument(
+    "-t", "--threads", metavar="", default="2", help="Define a number of threads [ Default 2 ]")
+parser.add_argument(
+    "-o", "--output", metavar="", default="result.txt", help="output in txt [ Default result.txt ]")
 args = parser.parse_args()
 
-
-def headersURL():
-
+def parser_file_urls(file):
     with open(file, 'r') as arquivo:
         dominios = arquivo.read().split('\n')[:-1]
-    with open('results.txt', 'w') as fp:
+        for dominio in dominios:
+            all_urls.append(dominio)
 
-        for url in dominios:
-            headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
-            try:
-                r = requests.get(url, headers=headers)
-                print(
-                    f"{Fore.RED}{r.headers['server'] if r.headers['server'] else 'none'}{Style.RESET_ALL}:{url}")
-                fp.write(
-                    f"{r.headers['server'] if r.headers['server'] else 'none'}:{url}\n")
-            except:
-                pass
+def headersURL():
+    while len(all_urls) != 0:
+        url = all_urls[0]
+        all_urls.remove(url)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+        try:
+            r = requests.get(url, headers=headers)
+            print(
+                f"{Fore.RED}{r.headers['server'] if r.headers['server'] else 'none'}{Style.RESET_ALL}:{url}")
+            string = f"{r.headers['server'] if r.headers['server'] else 'none'}:{url}"
+            save_urls(string)
+        except:
+            pass
+
+
+def save_urls(url):
+    with open(f"{args.output}", "a") as file:
+        file.write(url + "\n")
+
+
+def all_threads(number_threads):
+    controler = 0
+    for thread in range(0 + int(number_threads)):
+        Thread(target=headersURL).start()
+        controler += 1
 
 
 if __name__ == "__main__":
+    all_urls = [] # Controler for threads
+    file = args.file
+    number_of_threads = args.threads
     try:
-        file = sys.argv[1]
-        headersURL()
+        parser_file_urls(file)
+        all_threads(number_of_threads)
 
     except Exception as e:
         print(
